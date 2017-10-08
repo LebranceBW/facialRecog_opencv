aa98efb3e3f023c0c5a0f4c76029985a8c3c2a85
Lebranc <qinghuabw@163.com>
2017-10-08 16:11:40 +0800

    第二次重构

--- facialdetect/main_rewrite.cpp
+++ facialdetect/main_rewrite.cpp
@@ -33,6 +33,33 @@ Mat FaceEyeDetection(const Mat frame,vector<Rect> &face,vector<Rect> &eye)
 		cv::rectangle(*temp, r, cv::Scalar(0));
 	return *temp;
 }
+
+bool IsCorrect(vector<Rect> faceVector,vector<Rect> eyeVector)
+{
+	if ((faceVector.size() == 1) && (eyeVector.size() == 2)) //数量校验
+		if ((faceVector[0].x < eyeVector[0].x) && (faceVector[0].x < eyeVector[1].x) && (faceVector[0].y < eyeVector[0].y) && (faceVector[0].y < eyeVector[1].y))//眼睛在脸部之内
+			if ((abs(eyeVector[0].width - eyeVector[1].width) < 10) && (abs(eyeVector[0].height - eyeVector[1].height) < 10))
+				if (abs(eyeVector[0].y - eyeVector[1].y) < 10)
+					return true;
+	return false;
+}
+Mat FaceAlign(Mat raw,vector<Rect> faceVector, vector<Rect> eyeVector)
+{
+	const double SCALE = 1; //缩放比例
+	const int LENGTH = (int)sqrt(raw.cols*raw.cols + raw.rows*raw.rows) * SCALE; //旋转后的矩阵大小
+	Mat rotateMat(LENGTH, LENGTH, raw.type()); //变换后的矩阵
+	Rect ROIRect((LENGTH - raw.cols) / 2, (LENGTH - raw.rows) / 2, raw.rows, raw.cols);
+	Mat ROIMat(rotateMat, ROIRect); //对目标矩阵进行ROI选区
+	raw.copyTo(ROIMat);
+	//以上完成了拷贝图像的操作
+	cv::Point2f center(LENGTH / 2, LENGTH / 2);
+	double angle = atan((double)(eyeVector[0].y - eyeVector[1].y) / (eyeVector[0].x - eyeVector[1].x)) * 180 / CV_PI;
+	Mat transfromeMat = cv::getRotationMatrix2D(center, angle, SCALE);
+	Mat result(raw.rows,raw.cols, raw.type());
+	cv::warpAffine(rotateMat, result, transfromeMat, cv::Size(LENGTH,LENGTH));
+	return result;
+
+}
 int main()
 {
 	VideoCapture camera = CameraInit(); //初始化并设置摄像头
@@ -41,7 +68,6 @@ int main()
 	vector<Rect> faceVector, eyeVector;
 	cv::namedWindow("实时视频");
 	cv::namedWindow("旋转后的内容");
-	cv::namedWindow("面部内容");
 	faceCC.load(FACE_CASCADE_PATH);
 	eyeCC.load(EYE_CASCADE_PATH);
 	while (true)
@@ -49,31 +75,12 @@ int main()
 		camera >> rawFrame;
 		cv::cvtColor(rawFrame, grayFrame, cv::COLOR_RGB2GRAY);
 		cv::imshow("实时视频", FaceEyeDetection(grayFrame, faceVector, eyeVector));
-		if(flag&&(faceVector.size() == 1)&&(eyeVector.size() == 2))
-			if (((eyeVector[0].y - eyeVector[1].y) <= 15) || ((eyeVector[1].y - eyeVector[0].y) <= 15))
-			{
+		if(IsCorrect(faceVector,eyeVector))
+		{
 				Mat raw = (Mat(grayFrame, faceVector[0])).clone();
-				//flag = false;
-				Mat rotate;
-				
-				//const int ANGLE = 30;
-				const double SCALE = 1.1;
-				int length = sqrt(raw.cols*raw.cols + raw.rows*raw.rows) * SCALE;
-				Mat tempMat(length, length, raw.type());
-				Rect ROIRect((length / 2 - raw.cols / 2), (length / 2 - raw.cols / 2), raw.cols, raw.rows);
-				Mat temp2Mat(tempMat, ROIRect);
-				raw.copyTo(temp2Mat);
-				cv::Point2f center(length / 2, length / 2);
-
-				double angle = atan((double)(eyeVector[0].y - eyeVector[1].y) / (eyeVector[0].x - eyeVector[1].x)) * 180 / CV_PI;
-
-				Mat transfromeMat = cv::getRotationMatrix2D(center, angle, SCALE);
-				cv::warpAffine(tempMat,rotate,transfromeMat,cv::Size(length,length));
-			
-				
-				cv::imshow("面部内容", raw);
+				Mat rotate = FaceAlign(raw, faceVector, eyeVector);
 				cv::imshow("旋转后的内容",rotate);
-			}
+		}
 		if (-1 != cv::waitKey(1)) break;
 
 	}
